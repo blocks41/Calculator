1c2c70ac05628399b7f8962de6c7045f173fa9a8
block41 <xy.zou@qq.com>
2021-05-07 10:28:20 +0800

    update

--- Calculator3.0/main.cpp
+++ Calculator3.0/main.cpp
@@ -1,4 +1,4 @@
-//支持带括号的表达式,支持+-*/、^(指数)
+//支持带括号的表达式,支持+-*/、^(指数)、取负、!(阶乘)
 #include <iostream>
 #include <stack>
 #include <math.h>
@@ -7,6 +7,7 @@ using namespace std;
 
 double inStack();   //核心函数，将操作符有序的入栈计算， 最后返回结果
 void calculate(stack<char>& Ope, stack<double>& Num);  //用来计算加减乘除, 结果放在数字栈顶
+void factorial(stack<double>& Num);   //用来计算阶乘
 int priority(char ope_);   //用来计算操作符的优先级
 
 int main() {
@@ -37,7 +38,8 @@ double inStack() {
 				while (!Ope.empty()) calculate(Ope, Num); //如果符号栈不空，就一直计算
 				return Num.top();  //如果是等号且符号栈顶为空，就返回数字栈顶元素
 			}
-			else if (ope_ == '(' || Ope.empty()) Ope.push(ope_);    //如果符号是左括号或符号栈为空直接压入
+			else if (ope_ == '!') factorial(Num);  //如果是！就阶乘
+			else if (ope_ == '(' ||Ope.empty()) Ope.push(ope_);    //如果符号是左括号或符号栈为空直接压入
 			else if (ope_ == ')') {  //如果是右括号
 				while (Ope.top() != '(') calculate(Ope, Num); //一直计算完括号里的
 				Ope.pop();  //左括号出栈
@@ -53,16 +55,36 @@ double inStack() {
 
 void calculate(stack<char>& Ope, stack<double>& Num) {
 	double a, b;
-	a = Num.top(); Num.pop();
-	b = Num.top(); Num.pop();
 
-	if (Ope.top() == '+') Num.push(b + a);
-	else if (Ope.top() == '-') Num.push(b - a);
-	else if (Ope.top() == '*') Num.push(b * a);
-	else if (Ope.top() == '/') Num.push(b / a);
-	else if (Ope.top() == '^') Num.push(pow(b, a));
+	if (Ope.top() == '-') {
+		a = Num.top(); Num.pop();
+		Ope.pop();   //取出负号
+		Num.push(-a);  //压入负值
+		if (!Num.empty()) {
+			if(Ope.empty() || Ope.top() != '(')
+				Ope.push('+');  //如果前面还有数字，就压入+，即变成加负值
+		}
+	}
+	else {
+		a = Num.top(); Num.pop();
+		b = Num.top(); Num.pop();
 
-	Ope.pop();
+		if (Ope.top() == '+') Num.push(b + a);
+		else if (Ope.top() == '*') Num.push(b * a);
+		else if (Ope.top() == '/') Num.push(b / a);
+		else if (Ope.top() == '^') Num.push(pow(b, a));
+
+		Ope.pop();
+	}
+}
+
+void factorial(stack<double>& Num) {
+	int a = static_cast<int>(Num.top());
+	int result=1;
+
+	for (int i = 1; i <= a; i++) result *= i;
+
+	Num.push(static_cast<double>(result));
 }
 
 int priority(char ope_) {
